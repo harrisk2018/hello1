745d95d4019ee0c7ca834d282b538b28847b2aa5
harrisk2018 <kharris@sjnma.org>
2018-05-17 09:20:31 -0500

    Javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+alert("hello!");
+alert("do you have pizza");
+console.log("but gelato is ok");
+
+var person = prompt("please enter your name", "The dog went to the park ");
+var font = prompt("what is your favorite font?" ,"Comic Sans");
+var noun1 =  prompt("please enter a noun", "");
+var noun2 = prompt ("please enter a noun","");
+var noun3  = prompt("please enter a noun","");
+var noun4 =prompt("please enter a noun","");
+var verb = prompt("please enter a verb","");
+var verb2 = prompt("please enter a verb","");
+var verb3 =  prompt("please enter a verb","");
+var verb4 =  prompt("please enter a verb","");
+var adjective = prompt("please enter a adjective"," the dog walked to the park  ");
+
+document.getElementById("name").innerHTML= "The " + noun1 +verb+" to the" + noun2 + "." +" at the" + noun2 + "." + "and then the "  + verb2 + "then he went to jail" + noun3+ "."  + "."+ "then he went to get" + verb4+ "."   //   !how are you today?" + font + " what is your favotite font, apparently"; 
+ 
